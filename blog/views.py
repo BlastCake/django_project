dff11db1b118cd4195e00cc3ebab1855ac7fbaf5
Artem Aliev <artemaliev1993@gmail.com>
2019-07-05 15:56:39 +0300

    Added TagUpdate class in views

--- blog/views.py
+++ blog/views.py
@@ -7,11 +7,6 @@ from .forms import TagForm, PostForm
 # Create your views here.
 
 
-def posts_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/index.html', context={'posts': posts})
-
-
 class PostDetail(ObjectDetailMixin ,View):
     model = Post
     template = 'blog/post_detail.html'
@@ -22,10 +17,9 @@ class PostCreate(ObjectCreateMixin, View):
     template = 'blog/post_create_form.html'
 
 
-
-def tags_list(request):
-    tags = Tag.objects.all()
-    return render(request, 'blog/tags_list.html', context={'tags': tags})
+def posts_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
 
 
 class TagDetail(ObjectDetailMixin ,View):
@@ -37,3 +31,14 @@ class TagCreate(ObjectCreateMixin, View):
     form_model = TagForm
     template = 'blog/tag_create.html'
 
+
+class TagUpdate(View):
+    def get(self, request, slug):
+        tag = Tag.objects.get(slug__iexact=slug)
+        bound_form = TagForm(tag)
+        return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag': tag})
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
