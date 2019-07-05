2f8ad7cd382ad3cdc5b977d08b70f02d8bf8dc4c
artem <artemaliev1993@gmail.com>
2019-07-05 14:24:22 +0300

    Added function who generate slug. Create ObjectCreateMixin class who merged functionality both Creating classes.

--- blog/utils.py
+++ blog/utils.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from .models import *
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Tag
 
 class ObjectDetailMixin:
     # Создали этот класс так как во .views.py в классах PostDetail и TagDetail пратически идентичные методы get
@@ -8,4 +8,21 @@ class ObjectDetailMixin:
     def get(self, request, slug):
         obj = get_object_or_404(self.model, slug__iexact=slug)
         return render(request, self.template, context={self.model.__name__.lower(): obj})
+
+class ObjectCreateMixin:
+    form_model = None
+    template = None
+
+    def get(self, request):
+        form = self.form_model()
+        return render(request, self.template, context={'form': form})
+
+    def post(self, request):
+        bound_form = self.form_model((request.POST))
+
+        if bound_form.is_valid():
+            new_obj = bound_form.save()
+            return redirect(new_obj)
+
+        return render(request, self.template, context={'form': bound_form})
 
