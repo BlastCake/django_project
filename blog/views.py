e621c5e643b3465e8280711c2e9612765d68e3ec
artem <artemaliev1993@gmail.com>
2019-07-06 12:25:41 +0300

    Created class PostDelete. Fixed class TagDelete.

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View
 from django.urls import reverse
 from .utils import *
 from .forms import TagForm, PostForm
+from .models import *
 
 # Create your views here.
 
@@ -21,6 +22,12 @@ class PostUpdate(ObjectUpdateMixin, View):
     model = Post
     model_form = PostForm
     template = 'blog/post_update_form.html'
+
+
+class PostDelete(ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete_form.html'
+    redirect_url = 'posts_list_url'
 
 
 def posts_list(request):
@@ -44,15 +51,10 @@ class TagUpdate(ObjectUpdateMixin, View):
     template = 'blog/tag_update_form.html'
 
 
-class TagDelete(View):
-    def get(self, request, slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        return render(request, 'blog/tag_delete_form.html', context={'tag': tag})
-
-    def post(self, request, slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        tag.delete()
-        return redirect(reverse('tags_list_url'))
+class TagDelete(ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete_form.html'
+    redirect_url = 'tags_list_url'
 
 
 def tags_list(request):
