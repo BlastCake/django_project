6f43587f4c3a50faf6d2e47c65fa1dfe02b724d6
artem <artemaliev1993@gmail.com>
2019-07-08 12:21:10 +0300

    Created class PostDelete. Fixed class TagDelete.

--- blog/utils.py
+++ blog/utils.py
@@ -3,12 +3,12 @@ from django.urls import reverse
 
 
 class ObjectDetailMixin:
-    # Создали этот класс так как во .views.py в классах PostDetail и TagDetail пратически идентичные методы get
     model = None
     template = None
     def get(self, request, slug):
         obj = get_object_or_404(self.model, slug__iexact=slug)
-        return render(request, self.template, context={self.model.__name__.lower(): obj})
+        return render(request, self.template, context={self.model.__name__.lower(): obj, 'admin_object': obj,
+                                                       'detail': True})
 
 class ObjectCreateMixin:
     model_form = None
